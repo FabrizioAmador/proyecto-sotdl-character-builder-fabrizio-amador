8eeea645581d010196c2ccd6d31cbf0bcc403bd6
FabrizioAmador <amadorfabrizio@gmail.com>
2026-06-17 15:02:01 -0300

    Empezado el script. Iniciando funciones de lógica iniciales para modificar a los stats. Empezado el html de los linajes.

---
+++ script/script.js
@@ -0,0 +1,23 @@
+/*Esta será el cript raíz pero de momento será donde esté todo*/
+
+
+//Función para elegir personaje
+let eleccion = document.getElementById("elegirRaza");
+eleccion.addEventListener("click", function(){
+})
+
+
+
+//Pedir un stat
+function pedirStat (){
+    let statElegido = input("Eliga el stat a modificar: \n1. Fuerza\n2. Intelecto\n3. Agilidad\n4. Voluntad")
+}
+
+
+//Elegir stat a aumentar. (Modificación inicial de TODO personaje.)
+function modificarStatsBase(statAumentado, statRestado){
+        switch (statAumentado){
+        case "1":
+    }
+
+}
