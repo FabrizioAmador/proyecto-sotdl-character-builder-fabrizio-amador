828d1eb4ba0a7d591ce8331bcee52ee05f91d986
FabrizioAmador <amadorfabrizio@gmail.com>
2026-07-01 20:06:37 -0300

    Reemplazadas todas las barras invertidas \ por las normales / en todas las URLs. Terminado el archivo JSON con las muestras de los linajes a usar. Estructura básica de renderizado de la página de linajes empezado. Creado archivo de texto para anotaciones. Para terminar la aprovación del proyecto seria general el sessionstorage. Falta aplicar CSS en las cartas dinámicas ya creadas.

--- script/script.js
+++ script/script.js
@@ -1,27 +1,34 @@
 /*Esta será el script raíz pero de momento será donde esté todo*/
 
 
-//Para crear las cartas
+//= Para crear las cartas dinámicamente
+
+document.addEventListener('DOMContentLoaded', function(){
+    //Usar la funcion que que haga el renderizado.
+    renderizarLinaje();
+})
+
+//Funcion de renderizado
+function renderizarLinaje(){
 //Conectar con el json de los linajes
 fetch("/baseDatos/linajes.json")
 //Chequear que existan (Más para usarse si es con una API)
 .then(response => {
     if(!response.ok){
-        throw new Error("HTTP error! Status: ${response.status}")
+        throw new Error(`HTTP error! Status: ${response.status}`)
     } return response.json();
 }
 )
 .then(data=>{
     const contenedor = document.getElementById("contenedor__linajes");
     data.forEach(linaje => {
-        const cartaLinaje =
+        const cartaLinaje =`
         <div class="carta">
-            <img src="${linaje.imagen}" alt="linaje.nombre"></img>
+            <img src="${linaje.imagen}" alt="${linaje.nombre}"></img>
             <h3>${linaje.nombre}</h3>
             <p>${linaje.descripcion}</p>
-        </div>;
+        </div>`;
         contenedor.innerHTML += cartaLinaje;
     })
 })
-
-
+}
